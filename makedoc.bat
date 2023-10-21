980c11f6525c50cea8b677fcac99276d5b639200
David Yonge-Mallo <dlyongemallo@users.noreply.github.com>
2023-10-21 07:01:37 +0200

    WIP: debug windows environ

--- makedoc.bat
+++ makedoc.bat
@@ -1,3 +1,9 @@
+call doc\make.bat clean
 call doc\make.bat html
 pause
 start doc\_build\html\index.html
+pause
+call doc\make.bat clean
+call doc\make.bat latexpdf
+pause
+start doc\_build\latex\pyzx.pdf
